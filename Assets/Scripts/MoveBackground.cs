b9f29f550140549889ab0edd9330d4d90f8d5b56
MatiHaufn <m.zacharska@web.de>
2023-10-06 17:20:03 +0200

    Beginn mit HG scrollen, alles auf static gestellt

---
+++ Assets/Scripts/MoveBackground.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    [SerializeField] List<GameObject> _bgList = new List<GameObject>();
+
+    
+}
